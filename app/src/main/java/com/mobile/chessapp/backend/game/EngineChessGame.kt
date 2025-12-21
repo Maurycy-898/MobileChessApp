5444dc5e11714383a206a7ba36d09c0d41387a25
maurycyS <maurycys2001@gmail.com>
2025-12-21 21:10:43 +0100

    in progress

--- app/src/main/java/com/mobile/chessapp/backend/game/EngineChessGame.kt
+++ app/src/main/java/com/mobile/chessapp/backend/game/EngineChessGame.kt
@@ -5,29 +5,28 @@ import com.mobile.chessapp.backend.game.boardUtils.PieceColor
 import com.mobile.chessapp.backend.game.engineUtils.ChessEngine
 
 class EngineChessGame(
-    board: ChessBoard,
-    playerColor: PieceColor = PieceColor.WHITE,
-    oppColor: PieceColor = PieceColor.BLACK,
+  board: ChessBoard,
+  playerColor: PieceColor = PieceColor.WHITE,
+  oppColor: PieceColor = PieceColor.BLACK,
 ) : ChessGame(board, playerColor, oppColor) {
-    override fun prepareOpponentsTurn() {
-        onEngineMove()
-    }
 
-    override fun surrender() {
-        winner = oppColor
-        board.isGameOver = true
-    }
+  override fun prepareOpponentsTurn() {
+    onEngineMove()
+  }
 
-    private fun onEngineMove() {
-        if (oppColor == board.activeColor) {
-            val eval = ChessEngine.findBestMove(board)
-            val move = eval.bestMove
+  override fun surrender() {
+    winner = oppColor
+    board.isGameOver = true
+  }
 
-            if (move != null) {
-                board.doMove(move)
-                moveArchive.add(move)
-                boardUI.updateFields(board)
-            }
-        }
-    }
+  private fun onEngineMove() {
+    if (oppColor != board.activeColor) return
+
+    val eval = ChessEngine.findBestMove(board)
+    val move = eval.bestMove ?: return
+
+    board.doMove(move)
+    moveArchive.add(move)
+    boardUI.updateFields(board)
+  }
 }
--- app/src/main/java/com/mobile/chessapp/backend/game/engineUtils/ChessEngine.kt
+++ app/src/main/java/com/mobile/chessapp/backend/game/engineUtils/ChessEngine.kt
@@ -8,46 +8,55 @@ import com.mobile.chessapp.backend.game.moveUtils.MoveGenerator
 data class Evaluation(val bestMove: ChessMove?, val positionScore: Int)
 
 object ChessEngine {
-    fun findBestMove(board: ChessBoard, depth: Int = 3) : Evaluation {
-        return alfaBeta(board, alfa = LOSING_SCORE, beta = WINNING_SCORE, depth)
+  fun findBestMove(board: ChessBoard, depth: Int = 3): Evaluation {
+    return board.alfaBeta(alfa = LOSING_SCORE, beta = WINNING_SCORE, depth)
+  }
+
+  private fun ChessBoard.alfaBeta(
+    alfa: Int,
+    beta: Int,
+    depth: Int
+  ): Evaluation {
+
+
+    if (depth == 0) {
+      return Evaluation(null, (colorFactor * evaluate(this)))
     }
 
-    private fun alfaBeta(board: ChessBoard, alfa: Int, beta: Int, depth: Int) : Evaluation {
-        if (depth == 0) {
-            return Evaluation(bestMove = null, (board.activeColor.colorFactor*evaluate(board)))
-        }
-        var score: Int
-        var bestScore = Int.MIN_VALUE
-        var bestMove: ChessMove? = null
+    var score: Int
+    var bestScore = Int.MIN_VALUE
+    var bestMove: ChessMove? = null
 
-        val moves: List<ChessMove> = MoveGenerator.generateMoves(board)
-        if (moves.isEmpty()) {
-            val colFactor = board.activeColor.colorFactor
-            if (board.whiteKingAttacked) return Evaluation(bestMove = null, colFactor*LOSING_SCORE)
-            if (board.blackKingAttacked) return Evaluation(bestMove = null, colFactor*WINNING_SCORE)
-            return Evaluation(bestMove = null, DRAWING_SCORE)
-        }
+    val moves = MoveGenerator.generateMoves(this)
 
-        var currAlfa = alfa
-        for (move in moves) {
-            // move
-            board.doMove(move)
-            // evaluate
-            score = -alfaBeta(board, -beta, -currAlfa, (depth - 1)).positionScore
-            // undo move
-            board.undoMove(move)
+    if (moves.isEmpty()) return when {
+      blackKingAttacked -> colorFactor * LOSING_SCORE
+      whiteKingAttacked -> colorFactor * WINNING_SCORE
+      else -> DRAWING_SCORE
+    }.let { Evaluation(null, it) }
 
-            if (score >= beta) {
-                return Evaluation(move, beta)
-            }
-            if (score > bestScore) {
-                bestMove = move
-                bestScore = score
-                if (score > currAlfa) {
-                    currAlfa = score
-                }
-            }
+
+    var currAlfa = alfa
+    for (move in moves) {
+
+      doMove(move)
+      score = -this.alfaBeta(-beta, -currAlfa).positionScore
+      undoMove(move)
+
+      if (score >= beta) return Evaluation(move, beta)
+
+      if (score > bestScore) {
+        bestMove = move
+        bestScore = score
+
+        if (score > currAlfa) {
+          currAlfa = score
         }
-        return Evaluation(bestMove, bestScore)
+      }
     }
+
+    return Evaluation(bestMove, bestScore)
+  }
 }
+
+private val ChessBoard.colorFactor get() = activeColor.colorFactor
